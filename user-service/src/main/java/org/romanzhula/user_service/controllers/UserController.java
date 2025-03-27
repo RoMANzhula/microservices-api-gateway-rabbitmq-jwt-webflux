4ff5c686f6ce00bac2d246c9fc1b47690a07f41f
RoMANzhula <roman1985al@ukr.net>
2025-03-27 23:03:38 +0200

    Add UserController - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/controllers/UserController.java
@@ -0,0 +1,51 @@
+package org.romanzhula.user_service.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.controllers.responses.UserFeignResponse;
+import org.romanzhula.user_service.controllers.responses.UserResponse;
+import org.romanzhula.user_service.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/all")
+    public Flux<UserResponse> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return userService.getAll(page, size);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/{user-id}")
+    public Mono<ResponseEntity<UserResponse>> getUserById(
+            @PathVariable("user-id") String userId
+    ) {
+        return userService.getUserById(userId)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+        ;
+    }
+
+    @GetMapping("/by-username")
+    public Mono<ResponseEntity<UserFeignResponse>> getUserByUsername(
+            @RequestParam("username") String username
+    ) {
+        return userService.getUserByUsername(username)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+        ;
+    }
+
+}
