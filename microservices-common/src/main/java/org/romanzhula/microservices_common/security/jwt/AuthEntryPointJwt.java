4de83f1b1cb5b7dabfc788c458230861a27ecf3b
RoMANzhula <roman1985al@ukr.net>
2025-03-30 20:46:42 +0300

    Add AuthEntryPointJwt - microservices-common

---
+++ microservices-common/src/main/java/org/romanzhula/microservices_common/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,37 @@
+package org.romanzhula.microservices_common.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class AuthEntryPointJwt implements ServerAuthenticationEntryPoint {
+
+    @Override
+    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException authException) {
+        log.error("Unauthorized ERROR: {}", authException.getMessage());
+
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", exchange.getRequest().getPath().value());
+
+        return exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
+                .bufferFactory().wrap(body.toString().getBytes()))
+        );
+    }
+
+}
