a3bfc6b5fed5f9b7226c725180ef19017db0fa47
RoMANzhula <roman1985al@ukr.net>
2025-03-31 19:09:46 +0300

    Add TransactionConfig - wallet-service

---
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/configurations/TransactionConfig.java
@@ -0,0 +1,18 @@
+package org.romanzhula.wallet_service.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
+
+@Configuration
+public class TransactionConfig {
+
+    @Bean
+    public TransactionalOperator transactionalOperator(
+            ReactiveTransactionManager transactionManager
+    ) {
+        return TransactionalOperator.create(transactionManager);
+    }
+
+}
