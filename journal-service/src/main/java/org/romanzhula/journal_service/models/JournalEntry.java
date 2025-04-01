58433d16030fc466b7264e34092dd610bded82d6
RoMANzhula <roman1985al@ukr.net>
2025-04-01 16:58:32 +0300

    Add JournalEntry as model - journal-service - temporary without @Id annotation in all models (we check how work app vs r2dbc)

---
+++ journal-service/src/main/java/org/romanzhula/journal_service/models/JournalEntry.java
@@ -0,0 +1,32 @@
+package org.romanzhula.journal_service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Table("journal_entries")
+public class JournalEntry {
+
+    private Long id;
+
+    private UUID userId;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    public JournalEntry() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
