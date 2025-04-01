925946a04ab66ba736046f271c8742f499b30856
RoMANzhula <roman1985al@ukr.net>
2025-04-01 17:06:13 +0300

    Add JournalController - journal-service

---
+++ journal-service/src/main/java/org/romanzhula/journal_service/controllers/JournalController.java
@@ -0,0 +1,36 @@
+package org.romanzhula.journal_service.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.journal_service.responses.JournalResponse;
+import org.romanzhula.journal_service.services.JournalService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/journal")
+public class JournalController {
+
+    private final JournalService journalService;
+
+    @GetMapping("/all")
+    public Mono<ResponseEntity<Flux<JournalResponse>>> getAllEntries() {
+        Flux<JournalResponse> entries = journalService.getAllEntries();
+        return Mono.just(ResponseEntity.ok(entries));
+    }
+
+    @GetMapping("/{user-id}")
+    public Mono<ResponseEntity<Flux<JournalResponse>>> getAllUserJournalEntries(
+            @PathVariable("user-id") String userId
+    ) {
+        Flux<JournalResponse> entries = journalService.getAllUserJournalEntries(userId);
+        return Mono.just(ResponseEntity.ok(entries));
+    }
+
+}
