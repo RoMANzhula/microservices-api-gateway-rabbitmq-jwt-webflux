e42c8644f810dc256ad109f693a7cdad3d967963
RoMANzhula <roman1985al@ukr.net>
2025-04-01 22:26:09 +0300

    Add V1__Create_expenses_table.sql - expenses-service - flyway migration file

---
+++ expenses-service/src/main/resources/db/migration/V1__Create_expenses_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE expenses (
+    expense_id BIGSERIAL PRIMARY KEY,
+    user_id UUID NOT NULL,
+    expense_title TEXT,
+    amount NUMERIC(12, 2) NOT NULL,
+    message_request TEXT,
+    balance NUMERIC(12, 2) NOT NULL
+);
