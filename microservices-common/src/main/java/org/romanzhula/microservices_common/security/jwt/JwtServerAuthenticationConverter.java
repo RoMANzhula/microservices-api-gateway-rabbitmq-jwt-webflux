8f25d13e9f0d296efa9e07ad920d1c7769db392e
RoMANzhula <roman1985al@ukr.net>
2025-04-07 23:30:54 +0300

    Add JwtServerAuthenticationConverter - microservices-common

---
+++ microservices-common/src/main/java/org/romanzhula/microservices_common/security/jwt/JwtServerAuthenticationConverter.java
@@ -0,0 +1,29 @@
+package org.romanzhula.microservices_common.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtServerAuthenticationConverter implements ServerAuthenticationConverter {
+
+    private final CommonJWTService jwtService;
+    private static final String BEARER = "Bearer ";
+
+
+    @Override
+    public Mono<Authentication> convert(ServerWebExchange exchange) {
+        return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+                .filter(header -> header.startsWith(BEARER))
+                .map(header -> header.substring(BEARER.length()))
+                .map(JWToken::new)
+        ;
+    }
+
+}
