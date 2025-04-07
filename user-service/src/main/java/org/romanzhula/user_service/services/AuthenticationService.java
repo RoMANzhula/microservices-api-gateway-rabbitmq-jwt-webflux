69a650d827aca2e9f82be8e756283c25f233b154
RoMANzhula <roman1985al@ukr.net>
2025-04-07 22:57:49 +0300

    Up AuthenticationService - user-service - change all security logic

--- user-service/src/main/java/org/romanzhula/user_service/services/AuthenticationService.java
+++ user-service/src/main/java/org/romanzhula/user_service/services/AuthenticationService.java
@@ -2,8 +2,8 @@ package org.romanzhula.user_service.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.romanzhula.microservices_common.security.implementations.UserDetailsImpl;
 import org.romanzhula.microservices_common.security.jwt.CommonJWTService;
+import org.romanzhula.microservices_common.security.jwt.interfaces.TokenProvider;
 import org.romanzhula.user_service.controllers.requests.LoginRequest;
 import org.romanzhula.user_service.controllers.requests.RegistrationRequest;
 import org.romanzhula.user_service.controllers.responses.AuthResponse;
@@ -11,14 +11,15 @@ import org.romanzhula.user_service.models.User;
 import org.romanzhula.user_service.models.enums.UserRole;
 import org.romanzhula.user_service.repositories.UserRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 
 @Service
 @Slf4j
@@ -30,7 +31,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final RabbitTemplate rabbitTemplate;
     private final CommonJWTService jwtService;
-    private final ReactiveAuthenticationManager authenticationManager;
+    private final ReactiveUserDetailsService userDetailsService;
+    private final TokenProvider tokenProvider;
 
 
     public Mono<AuthResponse> userRegistration(RegistrationRequest registrationRequest) {
@@ -49,41 +51,32 @@ public class AuthenticationService {
                 userRepository.save(newUser)
                         .doOnSuccess(user -> rabbitTemplate.convertAndSend("user-created-queue", user))
                         .flatMap(savedUser -> {
-                            var userDetails = new UserDetailsImpl(
-                                    savedUser.getId().toString(),
-                                    savedUser.getUsername(),
-                                    savedUser.getPassword(),
-                                    List.of(savedUser.getRole().name())
-                            );
+                            User userDetails = User.builder()
+                                    .username(savedUser.getUsername())
+                                    .password(savedUser.getPassword())
+                                    .role(UserRole.USER)
+                                    .build()
+                            ;
 
-                            return jwtService.generateToken(userDetails)
-                                    .map(jwtToken -> AuthResponse.builder()
-                                            .token(jwtToken)
-                                            .build()
-                                    );
+                            String jwtToken = jwtService.generateToken((UserDetails) userDetails);
+
+                            return Mono.just(AuthResponse.builder()
+                                    .token(jwtToken)
+                                    .build())
+                            ;
                         })
                         .doOnError(error -> status.setRollbackOnly())
-        ).single();
+                ).single()
+        ;
     }
 
 
     public Mono<AuthResponse> login(LoginRequest loginRequest) {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                );
-
-        return authenticationManager.authenticate(authenticationToken)
-                .map(authentication -> (UserDetailsImpl) authentication.getPrincipal())
-                .flatMap(userDetails -> {
-                    return jwtService.generateToken(userDetails)
-                            .map(token -> AuthResponse.builder()
-                                    .token(token)
-                                    .build())
-                            ;
-                    }
-                )
+        return userDetailsService.findByUsername(loginRequest.getUsername())
+                .filter(u -> passwordEncoder.matches(loginRequest.getPassword(), u.getPassword()))
+                .map(tokenProvider::generateToken)
+                .map(AuthResponse::new)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED)))
         ;
     }
 
