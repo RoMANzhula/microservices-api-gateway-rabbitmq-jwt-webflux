4528f3f06371f71d96fa1fce25888a88dc0d9b41
RoMANzhula <roman1985al@ukr.net>
2025-04-08 21:39:49 +0300

    Up SecurityConfiguration - api-gateway-settings

--- api-gateway-settings/src/main/java/org/romanzhula/api_gateway_settings/configurations/SecurityConfiguration.java
+++ api-gateway-settings/src/main/java/org/romanzhula/api_gateway_settings/configurations/SecurityConfiguration.java
@@ -1,40 +1,41 @@
 package org.romanzhula.api_gateway_settings.configurations;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.romanzhula.microservices_common.cors.EnableCORS;
 import org.romanzhula.microservices_common.security.jwt.AuthEntryPointJwt;
-import org.romanzhula.microservices_common.security.jwt.AuthJWTFilter;
+import org.romanzhula.microservices_common.security.jwt.JwtAuthenticationManager;
+import org.romanzhula.microservices_common.security.jwt.JwtServerAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 
 @Configuration
-@Slf4j
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity
 @EnableCORS
 public class SecurityConfiguration {
 
     private final AuthEntryPointJwt authEntryPointJwtUnauthorizedHandler;
-    private final AuthJWTFilter authJwtFilter;
-    private final ReactiveAuthenticationManager reactiveAuthenticationManager;
 
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity httpSecurity,
+            JwtAuthenticationManager authenticationManager,
+            JwtServerAuthenticationConverter authenticationConverter
+    ) {
+
+        AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(authenticationManager);
+        authenticationWebFilter.setServerAuthenticationConverter(authenticationConverter);
+
+        return httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(authEntryPointJwtUnauthorizedHandler)
@@ -42,7 +43,6 @@ public class SecurityConfiguration {
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(
                                 "/api/v1/auth/**",
-                                "/api/v1/users/by-username",
                                 "/img/**",
                                 "/css/**",
                                 "/js/**"
@@ -51,30 +51,9 @@ public class SecurityConfiguration {
                 )
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .logout(ServerHttpSecurity.LogoutSpec::disable)
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-                .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
-                .addFilterBefore(authJwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-                .headers(headers -> headers
-                        .frameOptions(ServerHttpSecurity.HeaderSpec.FrameOptionsSpec::disable)
-                )
+                .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build()
         ;
-    }
-
-    @Bean
-    public AuthenticationWebFilter authenticationWebFilter() {
-        AuthenticationWebFilter filter = new AuthenticationWebFilter(reactiveAuthenticationManager);
-
-        filter
-                .setRequiresAuthenticationMatcher(
-                        ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, "/api/v1/auth/login")
-                )
-        ;
-
-        log.info("AuthenticationWebFilter configured for /api/v1/auth/login");
-
-        return filter;
     }
 
 }
