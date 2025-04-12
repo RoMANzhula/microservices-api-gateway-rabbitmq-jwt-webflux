e0ac751f5c94b349bdf02c37454e17684caec7c4
RoMANzhula <roman1985al@ukr.net>
2025-04-12 22:48:09 +0300

    Up Wallet model - wallet-service - fix jpa rules

--- wallet-service/src/main/java/org/romanzhula/wallet_service/models/Wallet.java
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/models/Wallet.java
@@ -1,6 +1,9 @@
 package org.romanzhula.wallet_service.models;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -10,8 +13,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table("wallets")
 public class Wallet {
 
+    @Id
+    @Column("wallet_id")
     private UUID userId;
 
     private BigDecimal balance;
