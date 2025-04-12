7c27dc25cf699c4e5cbc606770b333f4969ee5ac
RoMANzhula <roman1985al@ukr.net>
2025-04-12 22:53:12 +0300

    Up WalletRepository - wallet-service - fix problem with saving wallets

--- wallet-service/src/main/java/org/romanzhula/wallet_service/repositories/WalletRepository.java
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/repositories/WalletRepository.java
@@ -1,11 +1,21 @@
 package org.romanzhula.wallet_service.repositories;
 
 import org.romanzhula.wallet_service.models.Wallet;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends ReactiveCrudRepository<Wallet, UUID> {
+
+    @Modifying
+    @Query("UPDATE wallets SET balance = :balance WHERE wallet_id = :walletId")
+    Mono<Void> saveWallet(@Param("walletId") UUID walletId, @Param("balance") BigDecimal balance);
+
 }
