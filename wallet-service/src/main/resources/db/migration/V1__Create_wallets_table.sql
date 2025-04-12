f656e2ec2eb8f89f67f404509e832ea4c0ec3884
RoMANzhula <roman1985al@ukr.net>
2025-04-12 22:48:51 +0300

    Up V1__Create_wallets_table.sql - wallet-service - fix jpa rules

--- wallet-service/src/main/resources/db/migration/V1__Create_wallets_table.sql
+++ wallet-service/src/main/resources/db/migration/V1__Create_wallets_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE wallets (
-    user_id UUID PRIMARY KEY,
+    wallet_id UUID PRIMARY KEY,
     balance NUMERIC(12, 2) NOT NULL
 );
 
 COMMENT ON TABLE wallets IS 'Table for storing user wallets';
-COMMENT ON COLUMN wallets.user_id IS 'Unique user identity';
+COMMENT ON COLUMN wallets.wallet_id IS 'Unique user identity';
 COMMENT ON COLUMN wallets.balance IS 'User balance';
