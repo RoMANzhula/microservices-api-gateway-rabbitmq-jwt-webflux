274de36ff5e348b6df334592b55ce42521ed9243
RoMANzhula <roman1985al@ukr.net>
2025-04-14 22:54:25 +0300

    Up ApplicationConfiguration - wallet-service - add webclient connection

--- wallet-service/src/main/java/org/romanzhula/wallet_service/configurations/ApplicationConfiguration.java
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/configurations/ApplicationConfiguration.java
@@ -1,21 +1,32 @@
 package org.romanzhula.wallet_service.configurations;
 
+import lombok.RequiredArgsConstructor;
 import org.romanzhula.microservices_common.cors.CorsAutoConfiguration;
 import org.romanzhula.microservices_common.cors.CorsConfigurationProperties;
 import org.romanzhula.microservices_common.security.jwt.AuthEntryPointJwt;
 import org.romanzhula.microservices_common.security.jwt.CommonJWTService;
 import org.romanzhula.microservices_common.security.jwt.JwtAuthenticationManager;
 import org.romanzhula.microservices_common.security.jwt.JwtServerAuthenticationConverter;
+import org.romanzhula.microservices_common.utils.UserServiceWebClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfiguration {
 
     @Value("${app.jwt_secret_code}")
     private String secretKey;
+
+    @Value("${user-service.url}")
+    private String userServiceUrl;
 
     
     @Bean
@@ -46,6 +57,29 @@ public class ApplicationConfiguration {
     @Bean
     public CorsConfigurationProperties properties() {
         return new CorsConfigurationProperties();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService reactiveUserDetailsService(UserServiceWebClient userServiceWebClient) {
+        return username -> userServiceWebClient.getUserByUsername(username)
+                .map(userResponse -> User
+                        .withUsername(userResponse.getUsername())
+                        .password(userResponse.getPassword())
+                        .roles(userResponse.getRoles().toArray(new String[0]))
+                        .build()
+                )
+                .switchIfEmpty(Mono.error(new UsernameNotFoundException("User not found with username: " + username)))
+        ;
+    }
+
+    @Bean
+    public UserServiceWebClient userServiceWebClient() {
+        return new UserServiceWebClient(userServiceUrl);
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
 }
