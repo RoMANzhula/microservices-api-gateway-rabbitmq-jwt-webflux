da2473d1af327a8a88fd26d6fd67834b9e0f68de
RoMANzhula <roman1985al@ukr.net>
2025-04-15 16:45:14 +0300

    Up JournalService - journal-service - add checking with getPrincipal()

--- journal-service/src/main/java/org/romanzhula/journal_service/services/JournalService.java
+++ journal-service/src/main/java/org/romanzhula/journal_service/services/JournalService.java
@@ -4,8 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.romanzhula.journal_service.models.JournalEntry;
 import org.romanzhula.journal_service.repositories.JournalRepository;
 import org.romanzhula.journal_service.responses.JournalResponse;
+import org.romanzhula.microservices_common.dto.UserResponse;
+import org.romanzhula.microservices_common.utils.UserServiceWebClient;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -15,6 +20,7 @@ import java.util.UUID;
 public class JournalService {
 
     private final JournalRepository journalRepository;
+    private final UserServiceWebClient userServiceWebClient;
 
 
     public Flux<JournalResponse> getAllEntries() {
@@ -24,9 +30,13 @@ public class JournalService {
     }
 
     public Flux<JournalResponse> getAllUserJournalEntries(String userId) {
-        return journalRepository.findAllByUserId(UUID.fromString(userId))
-                .map(this::toJournalResponse)
-        ;
+        return getCurrentUserId().flatMapMany(currentUserId -> {
+            if (!currentUserId.equals(userId)) {
+                return Flux.error(new SecurityException("Access denied to journal entries of user with id: " + userId));
+            }
+
+            return journalRepository.findAllByUserId(UUID.fromString(userId)).map(this::toJournalResponse);
+        });
     }
 
     private JournalResponse toJournalResponse(JournalEntry journalEntry) {
@@ -36,6 +46,21 @@ public class JournalService {
                 journalEntry.getDescription(),
                 journalEntry.getCreatedAt()
         );
+    }
+
+    private Mono<String> getCurrentUserId() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(ctx -> {
+                    Object principal = ctx.getAuthentication().getPrincipal();
+                    if (principal instanceof User user) {
+                        return user.getUsername();
+                    } else {
+                        throw new IllegalStateException("Expected User but got: " + principal.getClass());
+                    }
+                })
+                .flatMap(userServiceWebClient::getUserByUsername)
+                .map(UserResponse::getId)
+        ;
     }
 
 }
