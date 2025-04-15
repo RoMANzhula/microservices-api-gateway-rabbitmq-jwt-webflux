0ad0fdb92ea8d09a740703eb17f9449522d7e7bc
RoMANzhula <roman1985al@ukr.net>
2025-04-15 16:43:14 +0300

    Up JournalController - journal-service - add PreAuthorize

--- journal-service/src/main/java/org/romanzhula/journal_service/controllers/JournalController.java
+++ journal-service/src/main/java/org/romanzhula/journal_service/controllers/JournalController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.romanzhula.journal_service.responses.JournalResponse;
 import org.romanzhula.journal_service.services.JournalService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,14 @@ public class JournalController {
 
     private final JournalService journalService;
 
+    @PreAuthorize("hasRole('ADMIN')") // same with "hasAuthority('ROLE_ADMIN')"
     @GetMapping("/all")
     public Mono<ResponseEntity<Flux<JournalResponse>>> getAllEntries() {
         Flux<JournalResponse> entries = journalService.getAllEntries();
         return Mono.just(ResponseEntity.ok(entries));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{user-id}")
     public Mono<ResponseEntity<Flux<JournalResponse>>> getAllUserJournalEntries(
             @PathVariable("user-id") String userId
