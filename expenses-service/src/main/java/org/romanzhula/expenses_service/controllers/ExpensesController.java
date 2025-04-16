7839bfa83294a47bdcd3edcf1b3479432067dd4e
RoMANzhula <roman1985al@ukr.net>
2025-04-16 14:24:48 +0300

    Up ExpensesController - expenses-service - add PreAuthorize

--- expenses-service/src/main/java/org/romanzhula/expenses_service/controllers/ExpensesController.java
+++ expenses-service/src/main/java/org/romanzhula/expenses_service/controllers/ExpensesController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.romanzhula.expenses_service.responses.ExpensesResponse;
 import org.romanzhula.expenses_service.services.ExpensesService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class ExpensesController {
     private final ExpensesService expensesService;
 
 
+    @PreAuthorize("hasAnyRole('USER, ADMIN')")
     @GetMapping("/{user-id}")
     public Mono<ResponseEntity<Flux<ExpensesResponse>>> getAllExpensesByUserId(
             @PathVariable("user-id") String userId
